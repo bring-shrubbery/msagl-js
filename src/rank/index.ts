624a8b6af34e505faeb0425c85b7661ea168aa31
Assaf Shemesh <assaf.sun@gmail.com>
2020-07-11 20:02:25 +0300

    Init - starting a new project

---
+++ src/rank/index.ts
@@ -0,0 +1,24 @@
+import { networkSimplex } from "./network-simplex";
+
+/*
+ * Assigns a rank to each node in the input graph that respects the "minlen"
+ * constraint specified on edges between nodes.
+ *
+ * This basic structure is derived from Gansner, et al., "A Technique for
+ * Drawing Directed Graphs."
+ *
+ * Pre-conditions:
+ *
+ *    1. Graph must be a connected DAG
+ *    2. Graph nodes must be objects
+ *    3. Graph edges must have "weight" and "minlen" attributes
+ *
+ * Post-conditions:
+ *
+ *    1. Graph nodes will have a "rank" attribute based on the results of the
+ *       algorithm. Ranks can start at any index (including negative), we'll
+ *       fix them up later.
+ */
+export function rank(g) {
+  networkSimplex(g);
+}
