1461968ecaeac9f768f90d17453317a9e19435d6
Assaf Shemesh <assaf.sun@gmail.com>
2020-07-19 20:22:39 +0300

    layout comments

--- src/layout.ts
+++ src/layout.ts
@@ -2,12 +2,14 @@ import { calculateLayers } from "./layers";
 import { GraphLayout } from "./models";
 
 export function runLayout(graph) {
-  // remove cycles
+  //**** remove edge cycles - reverse an edge in order to prevent the cycle ****//
 
-  // Divide the nodes into layers
+  //**** Divide the nodes into layers ****//
   const graphLayout: GraphLayout = calculateLayers(graph);
 
-  // Order
+  //**** Order ****//
+
+  //**** Restore edge cycles ****//
 
   console.log(graphLayout);
   return graph;
