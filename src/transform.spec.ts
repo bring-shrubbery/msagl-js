ff31520a8bfc1938d835136e74637cb75939e8b1
Lev Nachmanson <levnach@hotmail.com>
2020-12-16 18:51:37 -0800

    add a test

--- src/transform.spec.ts
+++ src/transform.spec.ts
@@ -1,6 +1,5 @@
-import { MultiplyPoint, PlaneTransformation } from "./utils/geometry/planeTransformation";
+import { PlaneTransformation } from "./utils/geometry/planeTransformation";
 import { Point } from "./utils/geometry/point"
-import { Multiply } from "./utils/geometry/planeTransformation"
 
 test("plane transform test", () => {
     let m = new PlaneTransformation();
@@ -12,7 +11,13 @@ test("plane transform test", () => {
         1, 2, 3, 4, 5, 6);
     let m1 = PlaneTransformation.getPlaneTransformation(
         2, 3, 4, 5, 6, 7);
-    let m1m0p = MultiplyPoint(Multiply(m1, m0), p)
-    let m1m0p_ = MultiplyPoint(m1, MultiplyPoint(m0, p))
+
+    let m1m0p = m1.Multiply(m0).MultiplyPoint(p)
+    let m1m0p_ = m1.MultiplyPoint(m0.MultiplyPoint(p))
     expect(Point.close(m1m0p, m1m0p_, 0.00001)).toBe(true)
+
+    let invm0 = m0.Inverse()
+    expect(invm0.Multiply(m0).IsIdentity()).toBe(true)
+    expect(m0.Multiply(invm0).IsIdentity()).toBe(true)
+
 });
--- src/utils/geometry/planeTransformation.ts
+++ src/utils/geometry/planeTransformation.ts
@@ -4,22 +4,22 @@ import { Point } from "./point";
 export class PlaneTransformation {
 
     // the identity transform
-    elements: number[][] = [[1, 0 , 0], [0, 1, 0]];
-    
+    elements: number[][] = [[1, 0, 0], [0, 1, 0]];
+
 
     // the matrix elements
     get Elements() { return this.elements; }
     // i,j th element
     getElem(i: number, j: number) { return this.elements[i][j]; }
 
-    setElem(i: number, j: number, v:number) { this.elements[i][j] = v; }
+    setElem(i: number, j: number, v: number) { this.elements[i][j] = v; }
 
 
-    
-    
+
+
     // Divid matrix by a matrix
     static Divide(m0: PlaneTransformation, m1: PlaneTransformation) {
-        return Multiply(m0, Inverse(m1));
+        return m0.Multiply(m1.Inverse());
     }
 
     IsIdentity(): Boolean {
@@ -30,7 +30,7 @@ export class PlaneTransformation {
             Point.closeD(this.elements[1][1], 1) &&
             Point.closeD(this.elements[1][2], 0);
     }
-        
+
 
 
     // returns the point of the matrix offset
@@ -44,18 +44,15 @@ export class PlaneTransformation {
         r.elements[1][0] = m10; r.elements[1][1] = m11; r.elements[1][2] = m12;
         return r;
     }
-}
-
-// Rotation matrix    
-export function Rotation(angle : number):PlaneTransformation {
-    let cos = Math.cos(angle);
-    let sin = Math.sin(angle);
-    return PlaneTransformation.getPlaneTransformation(cos, -sin, 0, sin, cos, 0);
-}
+    // Rotation matrix    
+    static Rotation(angle: number): PlaneTransformation {
+        let cos = Math.cos(angle);
+        let sin = Math.sin(angle);
+        return PlaneTransformation.getPlaneTransformation(cos, -sin, 0, sin, cos, 0);
+    }
 
 
-export function ScaleAroundCenterTransformation(xScale: number, yScale: number, center: Point):PlaneTransformation
-    {
+    static ScaleAroundCenterTransformation(xScale: number, yScale: number, center: Point): PlaneTransformation {
         /*var toOrigin = new PlaneTransformation(1, 0, -center.x, 0, 1, -center.y);
           var scaleTr = new PlaneTransformation(scale, 0, 0,
           0, scale, 0);
@@ -67,38 +64,38 @@ export function ScaleAroundCenterTransformation(xScale: number, yScale: number, 
         return PlaneTransformation.getPlaneTransformation(xScale, 0, dX * center.x, 0, yScale, dY * center.y);
     }
 
-// Point by matrix multiplication
-export function MultiplyPoint(m: PlaneTransformation, p: Point):Point {
-    if (m != null)
+    // Point by matrix multiplication
+    MultiplyPoint(p: Point): Point {
         return new Point(
-            m.getElem(0, 0) * p.x + m.getElem(0, 1) * p.y + m.getElem(0, 2),
-            m.getElem(1, 0) * p.x + m.getElem(1, 1) * p.y + m.getElem(1, 2)
-            );
-    return new Point(0,0);
-}
+            this.getElem(0, 0) * p.x + this.getElem(0, 1) * p.y + this.getElem(0, 2),
+            this.getElem(1, 0) * p.x + this.getElem(1, 1) * p.y + this.getElem(1, 2)
+        );
+    }
 
-   // matrix matrix multiplication
-export function Multiply(a: PlaneTransformation, b: PlaneTransformation):PlaneTransformation {
-    if (a != null && b != null)
-        return PlaneTransformation.getPlaneTransformation(
-            a.getElem(0, 0) * b.getElem(0, 0) + a.getElem(0, 1) * b.getElem(1, 0),
-            a.getElem(0, 0) * b.getElem(0, 1) + a.getElem(0, 1) * b.getElem(1, 1), 
-            a.getElem(0, 0) * b.getElem(0, 2) + a.getElem(0, 1) * b.getElem(1, 2) + a.getElem(0, 2),            
-            a.getElem(1, 0) * b.getElem(0, 0) + a.getElem(1, 1) * b.getElem(1, 0),
-            a.getElem(1, 0) * b.getElem(0, 1) + a.getElem(1, 1) * b.getElem(1, 1),
-            a.getElem(1, 0) * b.getElem(0, 2) + a.getElem(1, 1) * b.getElem(1, 2) + a.getElem(1, 2));
-    return null;
-}
+    // matrix matrix multiplication
+    Multiply(b: PlaneTransformation): PlaneTransformation {
+        if (b != null)
+            return PlaneTransformation.getPlaneTransformation(
+                this.getElem(0, 0) * b.getElem(0, 0) + this.getElem(0, 1) * b.getElem(1, 0),
+                this.getElem(0, 0) * b.getElem(0, 1) + this.getElem(0, 1) * b.getElem(1, 1),
+                this.getElem(0, 0) * b.getElem(0, 2) + this.getElem(0, 1) * b.getElem(1, 2) + this.getElem(0, 2),
+                this.getElem(1, 0) * b.getElem(0, 0) + this.getElem(1, 1) * b.getElem(1, 0),
+                this.getElem(1, 0) * b.getElem(0, 1) + this.getElem(1, 1) * b.getElem(1, 1),
+                this.getElem(1, 0) * b.getElem(0, 2) + this.getElem(1, 1) * b.getElem(1, 2) + this.getElem(1, 2));
+        return null;
+    }
 
  // returns the inversed matrix
-function Inverse(m:PlaneTransformation) {
-     let det = m.getElem(0,0) * m.getElem(1,1) - m.getElem(1,0) * m.getElem(0,1);
-     
-     let a00 = m.getElem(1,1) / det;
-     let a01 = -m.getElem(0,1) / det;
-     let a10 = -m.getElem(1,0) / det;
-     let a11 = m.getElem(0,0) / det;
-     let a02 = -a00 * m.getElem(0,2) - a01 * m.getElem(1,2);
-     let a12 = -a10 * m.getElem(0,2) - a11 * m.getElem(1,2);
-     return PlaneTransformation.getPlaneTransformation(a00, a01, a02, a10, a11, a12);
- }
+    Inverse() {
+        let det = this.getElem(0, 0) * this.getElem(1, 1) - this.getElem(1, 0) * this.getElem(0, 1);
+
+        let a00 = this.getElem(1, 1) / det;
+        let a01 = -this.getElem(0, 1) / det;
+        let a10 = -this.getElem(1, 0) / det;
+        let a11 = this.getElem(0, 0) / det;
+        let a02 = -a00 * this.getElem(0, 2) - a01 * this.getElem(1, 2);
+        let a12 = -a10 * this.getElem(0, 2) - a11 * this.getElem(1, 2);
+        return PlaneTransformation.getPlaneTransformation(a00, a01, a02, a10, a11, a12);
+    }
+}
+
