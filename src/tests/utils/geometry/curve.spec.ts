03a48718b319af384dc8055f645de7a672a0e4a0
Lev Nachmanson <levnach@hotmail.com>
2021-01-11 11:20:42 -1000

    more tests on intersections
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/tests/utils/geometry/curve.spec.ts
+++ src/tests/utils/geometry/curve.spec.ts
@@ -1,19 +1,22 @@
 import {LineSegment} from '../../../utils/geometry/lineSegment';
 import {Ellipse} from '../../../utils/geometry/ellipse';
 import {Point} from './../../../utils/geometry/point';
-import {Polyline} from './../../../utils/geometry/polyline';
 import {Curve} from './../../../utils/geometry/curve';
 import {PlaneTransformation} from './../../../utils/geometry/planeTransformation';
 
 function intersectOnDiameter(a: Point, b: Point) {
   const ls = LineSegment.mkLinePP(a, b);
   const circ = Ellipse.mkCircle(b.minus(a).length() / 2, Point.middle(a, b));
-  const xx = Curve.getAllIntersections(ls, circ, false);
+  let xx = Curve.getAllIntersections(ls, circ, false);
   expect(xx.length == 2).toBeTruthy();
   expect(Point.closeD(xx[0].x.minus(xx[1].x).length(), b.minus(a).length())).toBeTruthy();
   for (const x of xx) {
     expect(Point.closeDistEps(x.x, a) || Point.closeDistEps(x.x, b)).toBeTruthy();
   }
+  const rad = ls.length() / 2;
+  ls.translate(new Point(0, rad));
+  xx = Curve.getAllIntersections(ls, circ, false);
+  expect(xx.length > 0 && xx.length <= 2).toBeTruthy();
 }
 
 test('curve intersect line circle', () => {
@@ -26,6 +29,7 @@ test('curve intersect line circle', () => {
     const m = PlaneTransformation.rotation(angle);
     const ac = m.multiplyPoint(a);
     const bc = m.multiplyPoint(b);
+    console.log(i);
     intersectOnDiameter(ac, bc);
   }
 });
