9237996969d2af99243ce69c8d7e992f510428df
Lev Nachmanson <levnach@hotmail.com>
2021-01-11 10:27:12 -1000

    more tests on intersections
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

---
+++ src/tests/utils/geometry/curve.spec.ts
@@ -0,0 +1,31 @@
+import {LineSegment} from '../../../utils/geometry/lineSegment';
+import {Ellipse} from '../../../utils/geometry/ellipse';
+import {Point} from './../../../utils/geometry/point';
+import {Polyline} from './../../../utils/geometry/polyline';
+import {Curve} from './../../../utils/geometry/curve';
+import {PlaneTransformation} from './../../../utils/geometry/planeTransformation';
+
+function intersectOnDiameter(a: Point, b: Point) {
+  const ls = LineSegment.mkLinePP(a, b);
+  const circ = Ellipse.mkCircle(b.minus(a).length() / 2, Point.middle(a, b));
+  const xx = Curve.getAllIntersections(ls, circ, false);
+  expect(xx.length == 2).toBeTruthy();
+  expect(Point.closeD(xx[0].x.minus(xx[1].x).length(), b.minus(a).length())).toBeTruthy();
+  for (const x of xx) {
+    expect(Point.closeDistEps(x.x, a) || Point.closeDistEps(x.x, b)).toBeTruthy();
+  }
+}
+
+test('curve intersect line circle', () => {
+  const a = new Point(1, 0);
+  const b = new Point(2, 0);
+  intersectOnDiameter(a, b);
+  const degree = Math.PI / 180;
+  for (let i = 0; i < 90; i++) {
+    const angle = i * degree;
+    const m = PlaneTransformation.rotation(angle);
+    const ac = m.multiplyPoint(a);
+    const bc = m.multiplyPoint(b);
+    intersectOnDiameter(ac, bc);
+  }
+});
