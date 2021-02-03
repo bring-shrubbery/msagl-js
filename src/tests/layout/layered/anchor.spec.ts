247d8c323e2852a20bee663fc06e30be25336fdf
Lev Nachmanson <levnach@hotmail.com>
2021-02-03 11:44:38 -1000

    closer to Sugiyama scheme
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

---
+++ src/tests/layout/layered/anchor.spec.ts
@@ -0,0 +1,23 @@
+import { GeomNode } from './../../../layout/core/geomNode'
+import { Anchor } from './../../../layout/layered/anchor'
+import { CurveFactory } from './../../../math/geometry/curveFactory'
+import { SvgDebugWriter } from './../../../math/geometry/svgDebugWriter'
+import { Point } from './../../../math/geometry/point'
+import { Node } from './../../../structs/node'
+test('anchor poly', () => {
+  const boundary = CurveFactory.mkRectangleWithRoundedCorners(
+    100,
+    50,
+    10,
+    30,
+    new Point(12, 12),
+  )
+  const n = GeomNode.mkNode(boundary, new Node())
+  const w = n.width / 2
+  const h = n.height / 2
+  const anchor = Anchor.mkAnchor(w, w, h, h, n, 0)
+  const poly = anchor.polygonalBoundary
+  expect(poly == null).toBe(false)
+  SvgDebugWriter.dumpICurves('/tmp/anchorBound.svg', [poly, n.boundaryCurve])
+
+})
