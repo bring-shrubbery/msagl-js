d40da5d8b3c08a43d82a2a95d353085c459685ff
Lev Nachmanson <levnach@hotmail.com>
2021-06-28 13:18:30 -0700

    work on MDS
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/layoutPlatform/layout/mds/Transform.ts
+++ src/layoutPlatform/layout/mds/Transform.ts
@@ -5,8 +5,9 @@
     const sin: number = Math.sin(angle * (Math.PI / 180))
     const cos: number = Math.cos(angle * (Math.PI / 180))
     for (let i = 0; i < x.length; i++) {
-      x[i] = cos * x[i] + sin * y[i]
+      const t = cos * x[i] + sin * y[i]
       y[i] = cos * y[i] - sin * x[i]
+      x[i] = t
     }
   }
 }
