b4076fe936eb917af5e84c6cfb6e80ebc63f5134
Lev Nachmanson <levnach@hotmail.com>
2021-07-16 17:15:23 -0700

    add more tests to PivotMds
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/tests/layoutPlatform/layout/mds/PivotMDS.spec.ts
+++ src/tests/layoutPlatform/layout/mds/PivotMDS.spec.ts
@@ -1,4 +1,8 @@
+import {GeomGraph} from '../../../..'
+import {DrawingGraph} from '../../../../drawing/drawingGraph'
+import {GeomObject} from '../../../../layoutPlatform/layout/core/geomObject'
 import {PivotMDS} from '../../../../layoutPlatform/layout/mds/PivotMDS'
+import {SvgDebugWriter} from '../../../../layoutPlatform/math/geometry/svgDebugWriter'
 import {parseDotGraph} from '../../../../tools/dotparser'
 import {
   createGeometry,
@@ -6,6 +10,8 @@ import {
   labelRectFunc,
   outputGraph,
 } from '../layered/layeredLayout.spec'
+import {sortedList} from '../sortedBySizeListOfgvFiles'
+import {join} from 'path'
 
 test('graph with subgraphs', () => {
   const dg = parseDotGraph('src/tests/data/graphvis/clust.gv')
@@ -13,4 +19,38 @@ test('graph with subgraphs', () => {
   const layout = new PivotMDS(gg, null, () => 1)
   layout.run()
   outputGraph(gg, 'clustMDS')
+})
+
+function runLayout(fname: string) {
+  const dg = parseDotGraph(fname)
+  if (dg == null) return null
+  createGeometry(dg.graph, nodeBoundaryFunc, labelRectFunc)
+  const pivotLayout = new PivotMDS(
+    <GeomGraph>GeomObject.getGeom(dg.graph),
+    null,
+    () => 1,
+  )
+  pivotLayout.run()
+  return dg
+}
+
+test('layout all gv files with MDS', () => {
+  const path = 'src/tests/data/graphvis/'
+  let i = 0
+  for (const f of sortedList) {
+    if (f.match('big(.*).gv')) continue // the parser bug
+    //console.log(f)
+    if (i++ > 160) return
+    let dg: DrawingGraph
+    try {
+      dg = runLayout(join(path, f))
+    } catch (Error) {
+      console.log(f + ' error:' + Error.message)
+      expect(1).toBe(0)
+    }
+    if (dg != null) {
+      const t: SvgDebugWriter = new SvgDebugWriter('/tmp/' + f + '.svg')
+      t.writeGraph(GeomObject.getGeom(dg.graph) as GeomGraph)
+    }
+  }
 })
