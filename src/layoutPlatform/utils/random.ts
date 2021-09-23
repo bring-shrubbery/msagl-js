e0dc9102fab2b966d73cb047d058a97829d52bc4
Lev Nachmanson <levnach@hotmail.com>
2021-09-23 09:40:42 -0700

    fix in randomInt()

--- src/layoutPlatform/utils/random.ts
+++ src/layoutPlatform/utils/random.ts
@@ -1,4 +1,5 @@
 // if max is an integer then returns random in the range [0, max-1]
 export function randomInt(max: number) {
-  return Math.floor(Math.random()) % Math.floor(max)
+  const r = Math.random()
+  return Math.floor(r * max)
 }
