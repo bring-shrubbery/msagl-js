0ed27e626dd424ba2e7f80955085291ae1e1c154
Lev Nachmanson <levnach@hotmail.com>
2021-10-12 10:51:37 -0700

    work on assert comment

--- src/tools/commentOutAssert.py
+++ src/tools/commentOutAssert.py
@@ -1,15 +1,25 @@
 import sys
+import glob
+
 def findBalancedParent(off, data): 
     o = data.find("(", off)
+    
     if o <0 : return -1
+    char = data[o]
+    insidestring=False
     level = 1
     while level > 0 :
         o = o+1
-        if (data[o] == '(') :
+        if insidestring: 
+            if char=='\'': 
+                insidestring=False
+                continue
+        if (char == '(') :
              level = level +1
-        elif data[o] == ')' : 
+        elif char == ')' : 
              level = level - 1
-        
+        elif char == '\'':
+             insidestring = True
     return o  
 
 
@@ -21,31 +31,32 @@ def findAssert(offset, data) :
     return (off,findBalancedParent(off, data))
 
 
-n = len(sys.argv)
-if (n<2) :
-    print('too few args')
-    sys.exit()
-filename = sys.argv[1]
-print('file name = ' + filename)
-file = open(filename, "r")
-data= file.read()
-print(data)
-file.close()
-ret = ""
-offset = 0
-(start, end) = findAssert(offset, data )
-if start < 0 :  
-    sys.exit() # no need to override the file
-file = open(filename, "w")
-while (start >=0) :
-    file.write(data[offset:start] + "/*" + data[start:end+1] +  "*/")
-    offset = end+1
+def sub(filename):    
+    print('file name = ' + filename)
+    file = open(filename, "r")
+    data= file.read()
+    file.close()
+    offset = 0
     (start, end) = findAssert(offset, data )
-file.write(data[offset:len(data)]    )
-file.close()   
+    if start < 0 :  
+        return # no need to override the file
+    file = open(filename, "w")
+    while (start >=0) :
+        file.write(data[offset:start] + "/*" + data[start:end+1] +  "*/")
+        offset = end+1
+        (start, end) = findAssert(offset, data )
+    file.write(data[offset:len(data)]    )
+    file.close()   
 
  
+n = len(sys.argv)
+if (n<2) :
+    print('too few args: provide directory name')
+    sys.exit()
 
+files = glob.glob(sys.argv[1] + '/**/*.ts', recursive=True)
+for file in files :
+    sub (file)
 
 
        
