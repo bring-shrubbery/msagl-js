72f1f9158c1ff877db73c77b0640a42c1ba1eae1
Lev Nachmanson <levnach@hotmail.com>
2021-10-15 17:29:30 -0700

    update examples\layout for the new package
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- examples/layout/app.ts
+++ examples/layout/app.ts
@@ -1,18 +1,23 @@
-import { GeomGraph, SugiyamaLayoutSettings, LayeredLayout, CancelToken } from 'msagl-js';
-
-// Missing export
-import { Size } from 'msagl-js/dist/layoutPlatform/math/geometry/rectangle';
+import {
+  GeomGraph,
+  SugiyamaLayoutSettings,
+  LayeredLayout,
+  CancelToken,
+  Size,
+} from "msagl-js";
 
 function measureTextSize(str: string): Size {
   return new Size(str.length * 8 + 8, 20);
 }
 
 async function main() {
-  const resp = await fetch('https://gist.githubusercontent.com/mohdsanadzakirizvi/6fc325042ce110e1afc1a7124d087130/raw/ab9a310cfc2003f26131a7149950947645391e28/got_social_graph.json');
+  const resp = await fetch(
+    "https://gist.githubusercontent.com/mohdsanadzakirizvi/6fc325042ce110e1afc1a7124d087130/raw/ab9a310cfc2003f26131a7149950947645391e28/got_social_graph.json"
+  );
   const data = await resp.json();
 
   const nodeMap: any = {};
-  const g = GeomGraph.mk('graph', new Size(0, 0))
+  const g = GeomGraph.mk("graph", new Size(0, 0));
   for (const node of data.nodes) {
     nodeMap[node.id] = node;
     g.setNode(node.character, measureTextSize(node.character));
@@ -21,9 +26,9 @@ async function main() {
     g.setEdge(nodeMap[edge.source].character, nodeMap[edge.target].character);
   }
 
-  const layoutSettings = new SugiyamaLayoutSettings()
-  const layout = new LayeredLayout(g, layoutSettings, new CancelToken())
-  layout.run()
+  const layoutSettings = new SugiyamaLayoutSettings();
+  const layout = new LayeredLayout(g, layoutSettings, new CancelToken());
+  layout.run();
 
   console.log(layout);
 }
