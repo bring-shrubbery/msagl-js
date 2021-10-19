9e6c4d4ec881c7f3b8f23ae2fd96ebe297b018b4
Lev Nachmanson <levnach@hotmail.com>
2021-10-18 20:05:56 -0700

    play with renderer.ts
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- examples/layout/src/renderer.ts
+++ examples/layout/src/renderer.ts
@@ -1,7 +1,6 @@
 import {Deck, OrthographicView} from '@deck.gl/core'
 
-import {LayeredLayout, GeomNode, GeomGraph, Point} from 'msagl-js'
-import {PolyIntEdge} from 'msagl-js/dist/layoutPlatform/layout/layered/polyIntEdge'
+import {interpolateICurve, GeomNode, GeomGraph, Point, GeomEdge} from 'msagl-js'
 
 import NodeLayer from './layers/node-layer'
 import EdgeLayer from './layers/edge-layer'
@@ -29,64 +28,46 @@ export default class Renderer {
   update() {
     const {geomGraph: layout} = this
 
-    const centerY = this.geomGraph.boundingBox.center
+    const center = this.geomGraph.boundingBox.center
 
-    for (const e of this.geomGraph.edges()) {
-      const edgeCurve = e.curve
-      // TODO render edgeCurve
-      if (e.edgeGeometry.sourceArrowhead) {
-        const tip: Point = e.edgeGeometry.sourceArrowhead.tipPosition
-        const start: Point = e.edgeGeometry.curve.start
-        // TODO draw arrowhead from start to tip
-      }
-      if (e.edgeGeometry.targetArrowhead) {
-        const tip: Point = e.edgeGeometry.targetArrowhead.tipPosition
-        const end: Point = e.edgeGeometry.curve.end
-        // TODO draw arrowhead from end to tip
-      }
-    }
-    for (const n of this.geomGraph.shallowNodes()) {
-      const bc = n.boundaryCurve
-      // TODO - render bc
-    }
-    // const nodeLayer = new NodeLayer<GeomNode>({
-    //   id: 'nodes',
-    //   data: layout.IntGraph.nodes,
-    //   background: true,
-    //   getPosition: (n) => [n.center.x, n.center.y],
-    //   getText: (n) => n.id,
-    //   getBorderWidth: 1,
-    //   getSize: 14,
-    //   // @ts-ignore
-    //   sizeUnits: 'common',
-    //   sizeMaxPixels: 24,
-    //   _subLayerProps: {
-    //     // Background box is absolutely positioned
-    //     background: {
-    //       getSize: 1,
-    //       sizeScale: 1,
-    //       sizeMinPixels: 0,
-    //       sizeMaxPixels: Number.MAX_SAFE_INTEGER,
-    //     },
-    //   },
-    // })
+    const nodeLayer = new NodeLayer<GeomNode>({
+      id: 'nodes',
+      data: Array.from(this.geomGraph.shallowNodes()),
+      background: true,
+      getPosition: (n) => [n.center.x, n.center.y],
+      getText: (n) => n.id,
+      getBorderWidth: 1,
+      getSize: 14,
+      // @ts-ignore
+      sizeUnits: 'common',
+      sizeMaxPixels: 24,
+      _subLayerProps: {
+        // Background box is absolutely positioned
+        background: {
+          getSize: 1,
+          sizeScale: 1,
+          sizeMinPixels: 0,
+          sizeMaxPixels: Number.MAX_SAFE_INTEGER,
+        },
+      },
+    })
 
-    // const edgeLayer = new EdgeLayer<PolyIntEdge>({
-    //   id: 'edges',
-    //   data: layout.IntGraph.edges,
-    //   getPath: (e) => [e.curve.start, e.curve.end].map((p) => [p.x, p.y]),
-    //   getArrowSize: 12,
-    //   getArrowType: 'caret',
-    //   getWidth: 1,
-    //   opacity: 0.5,
-    // })
+    const edgeLayer = new EdgeLayer<GeomEdge>({
+      id: 'edges',
+      data: Array.from(this.geomGraph.edges()),
+      getPath: (e) => interpolateICurve(e.curve, 0.5).map((p) => [p.x, p.y]),
+      getArrowSize: 12,
+      getArrowType: 'caret',
+      getWidth: 1,
+      opacity: 0.5,
+    })
 
-    // this.deck.setProps({
-    //   initialViewState: {
-    //     target: [centerX, centerY, 0],
-    //     zoom: 0,
-    //   },
-    //   layers: [edgeLayer, nodeLayer],
-    // })
+    this.deck.setProps({
+      initialViewState: {
+        target: [center.x, center.y, 0],
+        zoom: 0,
+      },
+      layers: [edgeLayer, nodeLayer],
+    })
   }
 }
