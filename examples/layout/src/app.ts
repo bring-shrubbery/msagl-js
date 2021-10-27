9f4707ace4358edd6ce9a2ec28327f9621f0c31b
Lev Nachmanson <levnach@hotmail.com>
2021-10-27 15:16:32 -0700

    intended usage

--- examples/layout/src/app.ts
+++ examples/layout/src/app.ts
@@ -9,15 +9,40 @@ import {
   MdsLayoutSettings,
   layoutGraph,
 } from 'msagl-js'
-import {parseDotString} from 'drawing-msagl-js'
 import {loadDefaultGraph, loadDotFile} from './load-data'
 import {dropZone} from './drag-n-drop'
 import Renderer from './renderer'
+import {DrawingGraph} from 'drawing-msagl-js'
 
 const renderer = new Renderer()
 
 function measureTextSize(str: string): {width: number; height: number} {
   return {width: str.length * 8 + 8, height: 20}
+}
+
+function renderDrawingGraph(g: DrawingGraph) {
+  document.getElementById('graph-name').innerText = g.graph.id
+
+  const gg = new GeomGraph(g, new Size(0, 0))
+
+  for (const node of g.shallowNodes) {
+    const wh = measureTextSize(node.id)
+    const geomNode = new GeomNode(node)
+    geomNode.boundaryCurve = CurveFactory.mkRectangleWithRoundedCorners(
+      wh.width,
+      wh.height,
+      1,
+      1,
+      new Point(0, 0),
+    )
+  }
+  for (const edge of g.edges) {
+    new GeomEdge(edge)
+  }
+
+  const layoutSettings = new MdsLayoutSettings()
+  layoutGraph(gg, null, () => layoutSettings)
+  renderer.setGraph(gg)
 }
 
 function render(g: Graph) {
@@ -47,7 +72,7 @@ function render(g: Graph) {
 
 dropZone('drop-target', async (f: File) => {
   const graph = await loadDotFile(f)
-  render(graph)
+  renderDrawingGraph(graph)
 })
 ;(async () => {
   const g = await loadDefaultGraph()
