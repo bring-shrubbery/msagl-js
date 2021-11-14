c3d5614c3c9013bdb98e52f6a241424c3d262f96
Lev Nachmanson <levnach@hotmail.com>
2021-11-13 19:52:02 -1000

    upgrade the version
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

---
+++ test/layout/driver.spec.ts
@@ -0,0 +1,18 @@
+import {GeomGraph} from '../../src'
+import {layoutGeomGraph} from '../../src/layout/driver'
+import {SvgDebugWriter} from '../utils/svgDebugWriter'
+import {
+  parseDotGraph,
+  createGeometry,
+  nodeBoundaryFunc,
+  labelRectFunc,
+} from '../utils/testUtils'
+
+test('layoutGeomGraph', () => {
+  const dg = parseDotGraph('test/data/graphvis/clust.gv')
+  createGeometry(dg.graph, nodeBoundaryFunc, labelRectFunc)
+  const geomGraph = <GeomGraph>GeomGraph.getGeom(dg.graph)
+  layoutGeomGraph(geomGraph, null)
+  const t: SvgDebugWriter = new SvgDebugWriter('/tmp/test_layoutGeomGraph.svg')
+  t.writeGeomGraph(geomGraph)
+})
